e086484c6663adac6f7db180fc99353e9fc0305f
Jonnie <Carino-j1@ulster.ac.uk>
2025-09-16 22:13:48 +0100

    Fixed error when submitting. Added console logs, UI design changes + Prepping to use MailKit

--- BlazorApp1/obj/Debug/net9.0/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/obj/Debug/net9.0/BlazorApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c720fbb76e30bcb8824afb50879707364f7de8a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d16321efc6725e52aa1e5bc745d74131e1fd322")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
